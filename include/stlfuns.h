0c2cbf1509bfd371840eb88cfcc347c721161029
Justin <jdietz224@gmail.com>
2023-03-28 09:25:10 -0500

    Renamed some variables to make a bit more sense

--- include/stlfuns.h
+++ include/stlfuns.h
@@ -8,8 +8,8 @@
 namespace Stl
 {
     typedef std::array<float,3> Vertex;
-    constexpr std::size_t TRIANGLE_SIZE = 50;
-    constexpr std::size_t HEADER_SIZE = 80;
+    constexpr std::size_t TRIANGLE_BYTE_SIZE = 50;
+    constexpr std::size_t HEADER_BYTE_SIZE = 80;
 
     enum STL_File_Type
     {
@@ -26,7 +26,7 @@ namespace Stl
     struct StlObject
     {
         STL_File_Type filetype = invalid;
-        std::string header = std::string(HEADER_SIZE,'\0');
+        std::string header = std::string(HEADER_BYTE_SIZE,'\0');
         uint32_t n_triangles = 0;
         std::vector<Triangle> tris;
     };
@@ -47,7 +47,7 @@ namespace Stl
     {
         auto fileflags = std::ios::in;
         StlObject obj;
-        obj.header.reserve(HEADER_SIZE);
+        obj.header.reserve(HEADER_BYTE_SIZE);
 
         if (isBinary) {
             fileflags = fileflags | std::ios::binary;
@@ -59,7 +59,7 @@ namespace Stl
         Triangle tmp_tri;
 
         //Read the 80 byte header from the file.
-        stlfile.read(std::data(obj.header), HEADER_SIZE);
+        stlfile.read(std::data(obj.header), HEADER_BYTE_SIZE);
 
         //Now get the total number of triangles in the object
         //TODO: Get rid of all uses of sizeof() function? I'm extremely paranoid about alignment padding causing difficult-to-identify bugs.
@@ -71,7 +71,7 @@ namespace Stl
             //Read each triangle all at once as a 50-byte chunk of data.
             //TODO: Is there a way to both read and push-back the triangle vector at the same time?
             //i.e. do this without going through an intermediate tmp_triangle variable?
-            stlfile.read(reinterpret_cast<char*>(&tmp_tri), TRIANGLE_SIZE);
+            stlfile.read(reinterpret_cast<char*>(&tmp_tri), TRIANGLE_BYTE_SIZE);
             obj.tris.push_back(tmp_tri);
         }
 
